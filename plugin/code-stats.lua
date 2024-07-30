cd11d0efd26cd42292351dfcd89c29bd872c8d97
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-07-30 19:26:42 +0800

    :ambulance: Fix vim.b.xp == nil

--- plugin/code-stats.lua
+++ plugin/code-stats.lua
@@ -5,14 +5,13 @@ local augroup_id = vim.api.nvim_create_augroup("codestats", {})
 vim.api.nvim_create_autocmd({ "InsertCharPre" }, {
     group = augroup_id,
     callback = function()
-        vim.b.xp = vim.b.xp or 0
-        vim.b.xp = vim.b.xp + 1
+        vim.b.xp = (vim.b.xp or 0) + 1
     end,
 })
 vim.api.nvim_create_autocmd({ "BufWrite", "BufLeave" }, {
     group = augroup_id,
     callback = function()
-        codestats.add_xp(vim.o.filetype, vim.b.xp)
+        codestats.add_xp(vim.o.filetype, vim.b.xp or 0)
         vim.b.xp = 0
     end,
 })
