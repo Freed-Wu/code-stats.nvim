6340485802c354b0c77bc04b2ea37d147dea5bc5
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-11-13 02:09:09 +0800

    :adhesive_bandage: Limit version to lua 5.1

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,5 +2,5 @@
 set -e
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-eval "luarocks install --local --force assets/rockspecs/xml-1.1.3-1.rockspec STDCPP_LIBDIR=$(scripts/get-STDCPP_LIBDIR.nix)"
-eval "luarocks install --local --force lua-requests OPENSSL_INCDIR=$(scripts/get-OPENSSL_INCDIR.nix) OPENSSL_LIBDIR=$(scripts/get-OPENSSL_LIBDIR.nix)"
+eval "luarocks install --lua-version 5.1 --local --force assets/rockspecs/xml-1.1.3-1.rockspec STDCPP_LIBDIR=$(scripts/get-STDCPP_LIBDIR.nix)"
+eval "luarocks install --lua-version 5.1 --local --force lua-requests OPENSSL_INCDIR=$(scripts/get-OPENSSL_INCDIR.nix) OPENSSL_LIBDIR=$(scripts/get-OPENSSL_LIBDIR.nix)"
