fb9b4d6d842b43474a5c9cb7dcc47a10013e840f
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-07-23 16:26:05 +0800

    :hammer: Update scripts/update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,4 @@
 set -e
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-nix-shell --run 'luarocks install --lua-version 5.1 --local --force assets/rockspecs/xml-1.1.3-1.rockspec STDCPP_LIBDIR=$STDCPP_LIBDIR &&
-luarocks install --lua-version 5.1 --local --force lua-requests OPENSSL_INCDIR=$OPENSSL_INCDIR OPENSSL_LIBDIR=$OPENSSL_LIBDIR'
-luarocks install --lua-version 5.1 --local --force code-stats.nvim
+nix-shell --run 'luarocks install --lua-version 5.1 --local --force code-stats.nvim STDCPP_LIBDIR=$STDCPP_LIBDIR OPENSSL_INCDIR=$OPENSSL_INCDIR OPENSSL_LIBDIR=$OPENSSL_LIBDIR'
