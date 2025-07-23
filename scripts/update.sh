02106d433eaf0a13123fdbb87b879f850e83d2b6
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-07-23 16:50:01 +0800

    :hammer: Update scripts/update.sh again

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,5 @@
 set -e
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-nix-shell --run 'luarocks install --lua-version 5.1 --local --force code-stats.nvim STDCPP_LIBDIR=$STDCPP_LIBDIR OPENSSL_INCDIR=$OPENSSL_INCDIR OPENSSL_LIBDIR=$OPENSSL_LIBDIR'
+nix-shell --run 'luarocks install --lua-version 5.1 --local --force xml-temp STDCPP_LIBDIR=$STDCPP_LIBDIR &&
+luarocks install --lua-version 5.1 --local --force luasec 0.7.2-1 OPENSSL_INCDIR=$OPENSSL_INCDIR OPENSSL_LIBDIR=$OPENSSL_LIBDIR'
